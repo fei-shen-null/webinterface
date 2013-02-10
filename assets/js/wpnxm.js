bf38bfc0f0dace62ebc88f17b8a895f41f017119
jakoch <jakoch@web.de>
2013-02-10 13:23:13 +0100

    added browser-update.js locally, switch from async web request to async local request

--- assets/js/wpnxm.js
+++ assets/js/wpnxm.js
@@ -6,6 +6,7 @@ window.onload=function()
     try {if ($buoop.ol) $buoop.ol();}catch (e) {}
     var e = document.createElement("script");
     e.setAttribute("type", "text/javascript");
-    e.setAttribute("src", "http://browser-update.org/update.js");
+    <!--e.setAttribute("src", "http://browser-update.org/update.js");-->
+    e.setAttribute("src", "http://localhost/webinterface/assets/js/browser-update.js");
     document.body.appendChild(e);
 }
