4db776bc346b76c6242a51895218ea54198c1b98
jakoch <jakoch@web.de>
2013-02-10 14:15:40 +0100

    highlight current page in headline main menu (js&css)

--- assets/js/wpnxm.js
+++ assets/js/wpnxm.js
@@ -9,4 +9,16 @@ window.onload=function()
     <!--e.setAttribute("src", "http://browser-update.org/update.js");-->
     e.setAttribute("src", "http://localhost/webinterface/assets/js/browser-update.js");
     document.body.appendChild(e);
+
+    setCurrentPageActiveInMainMenu();
+}
+
+ <!-- highlight current page in headline main menu -->
+function setCurrentPageActiveInMainMenu() {
+  aObj = document.getElementsByClassName('main_menu')[0].getElementsByTagName('a');
+  for(i=0;i<aObj.length;i++) {
+    if(document.location.href.indexOf(aObj[i].href)>=0) {
+      aObj[i].className='active';
+    }
+  }
 }
