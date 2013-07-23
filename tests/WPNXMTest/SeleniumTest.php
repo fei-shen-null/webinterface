8eedd21a352b75bebf2e6bccccf43a201744c848
jakoch <jakoch@web.de>
2013-07-24 00:01:09 +0200

    skip test if selenium is not running

--- tests/WPNXMTest/SeleniumTest.php
+++ tests/WPNXMTest/SeleniumTest.php
@@ -3,8 +3,27 @@ class SeleniumBootstrapTest extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected function setUp()
     {
+        $this->isSeleniumAvailable();
+
         $this->setBrowser('chrome');
         $this->setBrowserUrl('http://www.google.com/');
+    }
+
+    public function isSeleniumAvailable()
+    {
+        $selenium_running = false;
+
+        $fp = @fsockopen('localhost', 4444);
+        if ($fp !== false) {
+            $selenium_running = true;
+            fclose($fp);
+        }
+
+        if($selenium_running === false) {
+             $this->markTestAsSkipped(
+                'Selenium is not running on localhost:4444. Please start Selenium.'
+             );
+        }       
     }
  
     public function testTitle()
