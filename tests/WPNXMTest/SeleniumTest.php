f18f6c642358391918872f9d58db856a5ee13394
jakoch <jakoch@web.de>
2013-07-24 14:38:28 +0200

    trying to set chrome

--- tests/WPNXMTest/SeleniumTest.php
+++ tests/WPNXMTest/SeleniumTest.php
@@ -1,11 +1,22 @@
 <?php
 class SeleniumBootstrapTest extends PHPUnit_Extensions_Selenium2TestCase
 {
+    public static $browsers = array(
+        array(
+            'browserName' => 'chrome',
+            )
+        )
+    );
+
     protected function setUp()
     {
+        parent::setUp();
+
         $this->isSeleniumAvailable();
 
-        $this->setBrowser(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM2_BROWSER);
+        $this->setHost(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_HOST);
+        $this->setPort(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_PORT);
+        $this->setBrowser(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_BROWSER);
         $this->setBrowserUrl(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_TESTS_URL);
     }
 
@@ -23,13 +34,13 @@ class SeleniumBootstrapTest extends PHPUnit_Extensions_Selenium2TestCase
              $this->markTestAsSkipped(
                 'Selenium is not running on localhost:4444. Please start Selenium.'
              );
-        }       
+        }
     }
- 
+
     public function testTitle()
     {
         $this->url('http://www.google.com/');
         $this->assertEquals('Google', $this->title());
     }
- 
+
 }
