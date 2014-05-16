423ae0bbc014cf77a29e275c271da0b917180780
jakoch <jakoch@web.de>
2014-05-16 21:03:05 +0200

    removed duplicate slashes

--- bootstrap.php
+++ bootstrap.php
@@ -58,12 +58,12 @@ if (!defined('WPNXM_DIR')) {
     } else {
         // Windows Paths
         define('WPNXM_DIR', dirname(dirname(dirname(__DIR__))));
-        define('WPNXM_WWW_DIR', WPNXM_DIR . '\www' . DS);
-        define('WPNXM_CONTROLLER_DIR', WPNXM_WWW_DIR . '\tools\webinterface\php\Controller' . DS);
-        define('WPNXM_COMPONENTS_DIR', WPNXM_WWW_DIR . '\tools\webinterface\php\Components' . DS);
-        define('WPNXM_HELPER_DIR', WPNXM_WWW_DIR . '\tools\webinterface\php\Helper' . DS);
-        define('WPNXM_VIEW_DIR', WPNXM_WWW_DIR . '\tools\webinterface\php\View' . DS);
-        define('WPNXM_DATA_DIR', WPNXM_WWW_DIR . '\tools\webinterface\php\data' . DS);
+        define('WPNXM_WWW_DIR', WPNXM_DIR . 'www' . DS);
+        define('WPNXM_CONTROLLER_DIR', WPNXM_WWW_DIR . 'tools\webinterface\php\Controller' . DS);
+        define('WPNXM_COMPONENTS_DIR', WPNXM_WWW_DIR . 'tools\webinterface\php\Components' . DS);
+        define('WPNXM_HELPER_DIR', WPNXM_WWW_DIR . 'tools\webinterface\php\Helper' . DS);
+        define('WPNXM_VIEW_DIR', WPNXM_WWW_DIR . 'tools\webinterface\php\View' . DS);
+        define('WPNXM_DATA_DIR', WPNXM_WWW_DIR . 'tools\webinterface\php\data' . DS);
     }
 
     // Web Path Constants -> "http://.."
