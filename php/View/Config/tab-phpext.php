41bffaee43d35a04a92350f3116f0efe5f62dbf8
jakoch <jakoch@web.de>
2014-08-03 17:58:31 +0200

    cleanup

--- php/View/Config/tab-phpext.php
+++ php/View/Config/tab-phpext.php
@@ -5,16 +5,15 @@
 </h2>
 
 To enable an extension, check it's checkbox. To disable an extension, uncheck it's checkbox. <small>Surprise, surprise!</small>
-<form id="phpExtensionsForm" class="phpextensions form-horizontal"
-      action="<?php echo WPNXM_WEBINTERFACE_ROOT; ?>index.php?page=config&amp;action=update_phpextensions"
-      method="post">
+<form id="phpExtensionsForm" class="phpextensions form-horizontal" method="post"
+      action="<?php echo WPNXM_WEBINTERFACE_ROOT; ?>index.php?page=config&amp;action=update_phpextensions">
+    
+    <fieldset id="phpExtensionsFormContent">
+    <?php echo $form; ?>
+    </fieldset>
 
-<fieldset id="phpExtensionsFormContent">
-<?php echo $form; ?>
-</fieldset>
-
-<div class="right">
-    <button type="reset" class="btn btn-danger">Reset</button>
-    <button type="submit" class="btn btn-success">Submit</button>
-</div>
+    <div class="right">
+        <button type="reset" class="btn btn-danger">Reset</button>
+        <button type="submit" class="btn btn-success">Submit</button>
+    </div>
 </form>
