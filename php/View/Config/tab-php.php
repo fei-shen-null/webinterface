d1558d71023e9606a82c1d45130532edd1c45556
jakoch <jakoch@web.de>
2016-02-05 01:16:49 +0100

    replaced class info with alert cleanups

--- php/View/Config/tab-php.php
+++ php/View/Config/tab-php.php
@@ -8,12 +8,11 @@ This shows the currently active PHP version. If multiple PHP versions are instal
 The editor allows modifications of existing values in your php.ini.
 Click on a bold section to expand all directives for that section.
 You might then click on the value to edit it.
-Pressing the enter key will saves the new value to your php.ini. Take care!
+Pressing the enter key will save the new value to your php.ini.
 Do not forget to restart the PHP daemon in order to let the new settings become alive!
 
-<div class="info">
-    You are editing <?php echo $ini['ini_file']; ?>
-    <br>You are only able to modify existing values.
+<div class="alert alert-info" role="alert">
+    You are editing <b><?php echo $ini['ini_file']; ?></b>
 </div>
 
 <table id="treeTable">
@@ -25,6 +24,8 @@ Do not forget to restart the PHP daemon in order to let the new settings become 
 </thead>
 
 <?php
+// render the php.ini array into the TreeTable
+// use class="editable" on values (jquery.jEditable)
 $i = 0;
 $nodeName = '';
 foreach ($ini['ini_array'] as $key => $value) {
