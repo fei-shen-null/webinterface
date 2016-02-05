288cbadccac1b9498c783448ae2b0d84e0615ad7
jakoch <jakoch@web.de>
2016-02-05 01:17:54 +0100

    use bootstrap panels for project and tools

--- php/View/Projects/index.php
+++ php/View/Projects/index.php
@@ -1,8 +1,16 @@
 <h2 class="heading">Projects and Tools</h2>
 
-    <div class="left-box">
-        <div class="cs-message">
-            <div class="cs-message-content-projects">
+<div class="col-md-10 content-centered">
+
+    <div class="col-md-6" id="left-box">
+
+        <!-- Projects Panel -->
+        <div class="panel panel-default">
+          <div class="panel-heading panel-heading-gray">
+            <div class="pull-left">
+                <h4>Projects <small>(<?php echo $numberOfProjects; ?>)</small></h4>
+            </div>
+            <div class="pull-right">
                 <?php if (FEATURE_1 == true) { // @todo feature-flag create new project dialog ?>
                     <a class="btn btn-default btn-sm new-project-btn-position floatright"
                        data-toggle="modal" data-target="#myModal"
@@ -10,24 +18,40 @@
                        New Project
                     </a>
                 <?php } ?>
-                 <?php if (FEATURE_4 == true) { // @todo feature-flag create new project dialog ?>
+                <?php if (FEATURE_4 == true) { // @todo feature-flag list-domains dialog ?>
                     <a class="btn btn-default btn-sm new-project-btn-position floatright"
                        data-toggle="modal" data-target="#myModal"
                        href="index.php?page=domains">
                        List Domains
                     </a>
                 <?php } ?>
-                <h2>Projects <small>(<?php echo $numberOfProjects; ?>)</small></h2>
-                <?php echo $listProjects; ?>
             </div>
+            <div class="clearfix"></div>
+          </div>
+          <div class="panel-body panel-body-gray">
+            <!-- list-group for projects -->
+            <?php echo $listProjects; ?>
+          </div>
         </div>
     </div>
 
-    <div class="right-box">
-        <div class="cs-message">
-            <div class="cs-message-content-projects">
-                <h2>Tools <small>(<?php echo $numberOfTools; ?>)</small></h2>
-                <?php echo $listTools; ?>
+    <div class="col-md-6" id="right-box">
+
+        <!-- Tools Panel -->
+        <div class="panel panel-default">
+          <div class="panel-heading panel-heading-gray">
+            <div class="pull-left">
+                <h4>Tools <small>(<?php echo $numberOfTools; ?>)</small></h4>
             </div>
+            <div class="clearfix"></div>
+          </div>
+          <div class="panel-body panel-body-gray">
+            <?php echo $listTools; ?>
+          </div>
         </div>
+
     </div>
+
+    <div class="clearfix"></div> <!-- cleafix for left and right box -->
+
+</div> <!-- ./col-md-10 >
