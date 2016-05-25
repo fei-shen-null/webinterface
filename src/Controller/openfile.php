09587e5dae3749e0d9285facda1dc94755570246
jakoch <jakoch@web.de>
2016-05-25 17:24:53 +0200

    fixed openfile

--- src/Controller/openfile.php
+++ src/Controller/openfile.php
@@ -10,38 +10,32 @@
 
 function index()
 {
+    global $request;
+
     /**
      * You need to append the parameter "file" to the URL, e.g. "openfile.php?file=nginx-access-log".
      * Other values include: "nginx-error-log", "php-error-log". See the switch for more.
      */
-    $file = isset($_GET['file']) ? $_GET['file'] : null;
+    $file = $request->get('file', null);
 
     switch ($file) {
         case 'nginx-access-log':
-            Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\access.log');
-            break;
+            return WPNXM\Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\access.log');
         case 'nginx-error-log':
-            Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\error.log');
-            break;
+            return WPNXM\Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\error.log');
         case 'php-error-log':
-            Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\php_error.log');
-            break;
+            return WPNXM\Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\php_error.log');
         case 'mariadb-error-log':
-            Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\mariadb_error.log');
-            break;
+            return WPNXM\Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\mariadb_error.log');
         case 'mariadb-log':
-            Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\mariadb.log');
-            break;
+            return WPNXM\Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\mariadb.log');
         case 'mongodb-log':
-            Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\mongodb.log');
-            break;
+            return WPNXM\Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\mongodb.log');            
         case 'postgresql-log':
-            Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\pgsql.log');
+            return WPNXM\Webinterface\Helper\OpenFile::openFile(WPNXM_DIR.'\logs\pgsql.log');
         default:
-            throw new InvalidArgumentException(
-                sprintf('The method %s() has no case statement for "%s".', __METHOD__, $file)
-            );
-            break;
+            $msg = sprintf('The method %s() has no case statement for "%s".', __METHOD__, $file);
+            throw new InvalidArgumentException($msg);
     }
 
     header('Location: '.WPNXM_WEBINTERFACE_ROOT.'index.php?page=overview');
