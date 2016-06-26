3406897a170e4cd2dcdaabd4f43fa6f471e5e8c0
jakoch <jakoch@web.de>
2016-06-26 03:36:11 +0200

    renamed WPNXM_VERSION to WPNXM_STACK_VERSION

--- tests/WPNXMTest/Selenium2FirefoxTest.php
+++ tests/WPNXMTest/Selenium2FirefoxTest.php
@@ -49,6 +49,6 @@ class Selenium2FirefoxTest extends \PHPUnit_Extensions_Selenium2TestCase
     public function testTitle()
     {
         $this->url('/');
-        $this->assertEquals('WPИ-XM Server Stack for Windows - @APPVERSION@', $this->title());
+        $this->assertEquals('WPИ-XM Server Stack for Windows - @STACK_VERSION@', $this->title());
     }
 }
